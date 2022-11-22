ab9d2613a05f60e8f8340efda50ca5731168a770
ezuoo <juho0003@naver.com>
2022-11-22 22:54:34 +0900

    first terraform

--- 01-start/main.tf
+++ 01-start/main.tf
@@ -1,3 +1,38 @@
-terraform {
- 
+provider "local" { }
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+resource "local_file" "foo" {
+    filename = "${path.module}/foo.txt"
+    content  = "Hello World!"
+}
+
+data "local_file" "bar" {
+    filename = "${path.module}/bar.txt"
+}
+
+# output "file_bar" {
+#   value = data.local_file.bar
+# }
+
+# Create a VPC
+resource "aws_vpc" "foo" {
+  # cidr_block = "10.0.0.0/16"
+  cidr_block = "10.123.0.0/16"
+
+  tags = {
+    "Name" = "This is test vpc" 
+  }
+}
+
+# output "vpc_foo" {
+#   value = aws_vpc.foo
+# }
+
+data "aws_vpcs" "this" {}
+
+output "vpcs" {
+  value = data.aws_vpcs.this
 }
