7c96d892fef6c9fd4ec6b47afa0bb4a82d37b6f5
ezuoo <juho0003@naver.com>
2022-11-26 14:56:09 +0900

    add aws ncal rule

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -8,17 +8,20 @@ resource "aws_vpc" "default-vpc" {
   instance_tenancy = "default"
 
   tags = {
-    Name = "fastcampus-default-vpc"
+    Name = "default-vpc"
   }
 }
 
 # Creating aws vpc public subnet
 resource "aws_subnet" "default-vpc-public-subnet" {
-  vpc_id     = aws_vpc.default-vpc.id
-  cidr_block = "10.0.0.0/24"
+  vpc_id                  = aws_vpc.default-vpc.id
+
+  cidr_block              = "10.0.0.0/24"
+  availability_zone = "ap-northeast-2a"
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "fastcampus-default-vpc-public-subnet"
+    Name = "default-vpc-public-subnet"
   }
 }
 
@@ -28,7 +31,7 @@ resource "aws_subnet" "default-vpc-private-subnet" {
   cidr_block = "10.0.1.0/24"
 
   tags = {
-    Name = "fastcampus-default-vpc-private-subnet"
+    Name = "default-vpc-private-subnet"
   }
 }
 
@@ -36,7 +39,7 @@ resource "aws_subnet" "default-vpc-private-subnet" {
 resource "aws_internet_gateway" "default-vpc-igw" {
   vpc_id = aws_vpc.default-vpc.id
   tags = {
-    Name = "fastcampus-default-vpc-igw"
+    Name = "default-vpc-igw"
   }
 }
 
@@ -49,7 +52,7 @@ resource "aws_route_table" "default-vpc-route-public" {
   }
 
   tags = {
-    Name = "fastcampus-default-vpc-route-table-public"
+    Name = "default-vpc-route-table-public"
   }
 }
 
@@ -58,15 +61,6 @@ resource "aws_route_table_association" "default-vpc-route-public-subnet" {
   subnet_id      = aws_subnet.default-vpc-public-subnet.id
   route_table_id = aws_route_table.default-vpc-route-public.id
 }
-
-# Data :  aws vpc network acls's ids
-# data "aws_network_acls" "vpc_acls" {
-#   vpc_id = aws_vpc.default-vpc.id
-# }
-
-# output "acls" {
-#   value = data.aws_network_acls.vpc_acls
-# }
 
 # Creating aws vpc public network acl
 resource "aws_network_acl" "default-vpc-public-network-acl" {
@@ -79,8 +73,8 @@ resource "aws_network_acl" "default-vpc-public-network-acl" {
   }
 }
 
-# Creating aws vpc public network acl rule
-resource "aws_network_acl_rule" "default-vpc-public-network-acl-rule" {
+# Creating aws vpc public network acl rule : ssh
+resource "aws_network_acl_rule" "network-acl-rule-inbound-ssh" {
   network_acl_id = aws_network_acl.default-vpc-public-network-acl.id
 
   rule_number = 100
@@ -92,3 +86,50 @@ resource "aws_network_acl_rule" "default-vpc-public-network-acl-rule" {
   to_port     = 22
 
 }
+
+# Creating aws vpc public network acl rule : http
+resource "aws_network_acl_rule" "network-acl-rule-inbound-http" {
+  network_acl_id = aws_network_acl.default-vpc-public-network-acl.id
+
+  rule_number = 200
+  egress      = false
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = "0.0.0.0/0"
+  from_port   = 80
+  to_port     = 80
+
+}
+
+# Creating aws vpc public network acl rule : https
+resource "aws_network_acl_rule" "network-acl-rule-inbound-https" {
+  network_acl_id = aws_network_acl.default-vpc-public-network-acl.id
+
+  rule_number = 300
+  egress      = false
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = "0.0.0.0/0"
+  from_port   = 443
+  to_port     = 443
+
+}
+
+resource "aws_network_acl_rule" "network-acl-rule-outbound" {
+  network_acl_id = aws_network_acl.default-vpc-public-network-acl.id
+
+  rule_number = 100
+  egress      = true
+  protocol    = "tcp"
+  rule_action = "allow"
+  cidr_block  = "0.0.0.0/0"
+  # from_port   = "1024-65535"
+  # to_port     = "1024-65535"
+
+}
+
+# output "nacl-rule-outbound" {
+#   value = aws_network_acl_rule.network-acl-rule-outbound
+# }
+
+
